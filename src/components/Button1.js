8f071fdfe7443f0a9f054f179c5a2e8f753f3306
NethminiSandunika <nethminid92@gmail.com>
2023-04-24 08:49:07 +0530

    Update Button1.js

--- src/components/Button1.js
+++ src/components/Button1.js
@@ -1,1 +1,37 @@
-<Text>Hello World</Text>
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Button as PaperButton } from 'react-native-paper'
+import { theme } from '../core/theme'
+
+export default function Button1({ mode, style, ...props }) {
+  return (
+    <PaperButton
+      style={[
+        styles.button,
+        mode === 'outlined' && { backgroundColor: '#02585E' },
+        style,
+      ]}
+      labelStyle={styles.text}
+      mode={mode}
+      {...props}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: '70%',
+    marginVertical: 15,
+    paddingVertical: 2,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 0,
+    
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    lineHeight: 26,
+    
+  },
+})
